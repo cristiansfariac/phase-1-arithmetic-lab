2d4baac19fd99bd903dadc346bf672dd520ce617
Cristian Sfariac <cristian.sfariac@protonmail.com>
2022-01-31 20:22:33 -0500

    Done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1, num2 
+num1 = 2;
+num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random
+random >= 0;
+random = Math.floor(Math.random() * 10);
+
+let num3, num4
+num3 = 8;
+num4 = 4;
+let mod = num3 - num4;
+
+let max
+max = Math.max(20);
+ 
